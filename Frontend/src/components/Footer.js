bbb128139da1fb0ed20204b6ed4a8003262f9541
Anannya Mahajan <anannyamahajan00@gmail.com>
2025-09-12 22:47:16 +0530

    Delete Frontend/src directory

--- Frontend/src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Footer() {
-  return (
-    <footer className="text-center py-6 text-gray-500 text-sm bg-white shadow-inner">
-      © 2025 HealthNE Pro — Built for Rural Healthcare Excellence
-    </footer>
-  );
-}
