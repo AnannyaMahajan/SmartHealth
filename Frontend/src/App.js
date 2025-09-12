280fce52bdb9f2e20f255a26d470e7e8d973d0ba
Anannya Mahajan <anannyamahajan00@gmail.com>
2025-09-12 22:49:14 +0530

    Create App.js

---
+++ Frontend/src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Features from "./components/Features";
+import ConsentForm from "./components/ConsentForm";
+import VideoCall from "./components/VideoCall";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Hero />
+      <Features />
+      <ConsentForm />
+      <VideoCall />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
